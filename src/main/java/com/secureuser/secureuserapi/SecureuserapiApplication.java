56b26d3e4390a734bbeedaf3f04815d2a8c2fb82
Andres Galeano <afgaleano91@misena.edu.co>
2025-04-21 01:52:44 -0500

    create basic structure

---
+++ src/main/java/com/secureuser/secureuserapi/SecureuserapiApplication.java
@@ -0,0 +1,13 @@
+package com.secureuser.secureuserapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureuserapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureuserapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/secureuser/secureuserapi/SecureuserapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secureuser.secureuserapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureuserapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
